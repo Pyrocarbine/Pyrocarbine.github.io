f87d2ead195aeb867c530bce021896e38a82ecbc
Pyrocarbine <aaronhan2006@gmail.com>
2025-02-15 17:58:49 -0800

    Added Profile Image

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 function create_email() {
-    var email_address = "aaronhan2006@gmail.com";
+    var email_address = "a45han@uwaterloo.ca";
     var email_href = "mailto:" + email_address + "?" + "subject=" + "&" + "body=";
     wndMail = window.open(email_href, "_blank", "scrollbars=yes,resizable=yes,width=10,height=10");
-    
-  }
+}
   
 const button = document.getElementById("Email");
   
